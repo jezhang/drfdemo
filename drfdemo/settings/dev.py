f65e3db46c4bd1e519b4abe4364ea47deaca61a4
jean.zhang <jean.zhang@dbschenker.com>
2021-10-13 09:51:27 +0800

    first commit

---
+++ drfdemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-p0jd0gmp*#5f497f72a0b1=n5iq@7j16$$-b1v%9li4le&a12t'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
